fe215cfa9965959b222a9d8a39cc0c61a0126cfa
Elisabetta Scinto <elisabettascinto@Elisabettas-Air.home>
2024-04-25 12:59:31 +0200

    first commit

--- contactUs.js
+++ contactUs.js
@@ -1,16 +1,21 @@
+let okName = "";
+
 function validateForm() {
 
-    let name = document.getElementById('name').value;
+
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
     let phone = document.getElementById('phone').value
 
     if (name === "") {
         document.getElementById('error-message').classList.add('error-message-red');
+        console.log("please introduce your name");
     } else if (email=== "") {
         document.getElementById('error-message').classList.add('error-message-red');
+        console.log("please introduce email");
     } else if (phone === "") {
-        document.getElementById('error-message').classList.add('error-message-red');
+        document.getElementById('error-message').classList.add('error-message-red');  
+        console.log("please introduce the phone number");
     } else if (message=== "") {
         alert("Please insert at least 5 characters");
     } else {
@@ -18,7 +23,50 @@ function validateForm() {
     }
 }
 
+function validateName(){
+    let name = document.getElementById('name').value;
+    if (name === "") {
+        document.getElementById("error-name").hidden = true;
+        okName= false;
+    } else {
+        document.getElementById("error-name").hidden = false;
+        okName = name;
+        console.log(okName);
+    }
+}
+function validateEmail(){
+    let name = document.getElementById('email').value;
+    if (name === "") {
+        document.getElementById("error-name").innerText = "Please enter a valid e-mail"
+    } else {
+        okName = name;
+        console.log(okName);
+    }
+
+}
+
+function validatePhone(){
+    let name = document.getElementById('phone').value;
+    if (name === "") {
+        document.getElementById("error-name").innerText = "Please enter a valid phone number"
+    } else {
+        okName = name;
+        console.log(okName);
+    }
+
+}
+function validateMessage(){
+    let name = document.getElementById('message').value;
+    if (name === "") {
+        document.getElementById("error-name").innerText = "Please write at least 5 characters"
+    } else {
+        okName = name;
+        console.log(okName);
+    }
+
+}
 document.getElementById("myForm").addEventListener('submit', (event) => {
     event.preventDefault();
     validateForm();
+  
 })
